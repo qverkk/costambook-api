c3ec3dff058b1f977e603aa3d27c365426804e0f
qverkk <membersy@gmail.com>
2020-03-12 16:06:57 +0100

    Add web config and enable swagger

--- src/main/kotlin/com/qverkk/costambookapi/CostambookApiApplication.kt
+++ src/main/kotlin/com/qverkk/costambookapi/CostambookApiApplication.kt
@@ -1,23 +1,52 @@
 package com.qverkk.costambookapi
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
 import org.springframework.security.crypto.password.PasswordEncoder
-
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
-class CostambookApiApplication {
-	@Bean
-	fun passwordEncoder(): PasswordEncoder? {
-		return BCryptPasswordEncoder()
-	}
+@EnableSwagger2
+@EnableWebSecurity
+class CostambookApiApplication : WebSecurityConfigurerAdapter() {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder? {
+        return BCryptPasswordEncoder(11)
+    }
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/swagger-ui").permitAll()
+                .antMatchers("/user/login*", "/user/register*").permitAll()
+    }
+
+    @Bean
+    fun authProvider(): DaoAuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService)
+        authProvider.setPasswordEncoder(passwordEncoder())
+        return authProvider
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService? {
+        return super.userDetailsService()
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<CostambookApiApplication>(*args)
+    runApplication<CostambookApiApplication>(*args)
 }
